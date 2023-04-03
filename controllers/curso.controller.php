74a32e6fad4c9285d08b92705a2a85f706f89c2b
Ana Sotelo <Sotelocardenasa@gmail.com>
2023-04-03 13:44:54 -0500

    Guardando proyecto

--- controllers/curso.controller.php
+++ controllers/curso.controller.php
@@ -18,6 +18,9 @@ if (isset($_POST['operacion'])){
     
     //Enviamos el resultado a la vista como un JSON
     echo json_encode($resultado);
-     
+  } 
+  
+  if($_POST['operacion'] == 'buscarID'){
+
   }
 }
